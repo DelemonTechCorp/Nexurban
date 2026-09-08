7c5ebdf42dbbfc6a0625966cbda67dbbe0bd6ae2
cthridya <cthridya@gmail.com>
2026-09-08 16:21:29 +0530

    update

--- nexurban/settings.py
+++ nexurban/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 X_OPPERP_API_KEY = config("X_OPPERP_API_KEY")
