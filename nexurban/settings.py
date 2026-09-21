3b8d6a8a307885e0a4c2fbe6a8617105576ffa18
cthridya <cthridya@gmail.com>
2026-09-21 09:58:29 +0530

    done

--- nexurban/settings.py
+++ nexurban/settings.py
@@ -24,10 +24,10 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-01*=mzemn!rki@b-8sx4k7^e5uw&q^52+$hrm)ry^_fb+oi1k('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
+# SECRET_KEY = 'django-insecure-01*=mzemn!rki@b-8sx4k7^e5uw&q^52+$hrm)ry^_fb+oi1k('
 
 # ALLOWED_HOSTS = []
 
@@ -83,12 +83,6 @@ WSGI_APPLICATION = 'nexurban.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': [
@@ -109,6 +103,12 @@ CKEDITOR_5_CONFIGS = {
     },
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
